7b6c51f8d48dad59bb0d51c2fb0ce710a0f6a0f4
gusrqueque <gusrqueque@gmail.com>
2023-05-03 00:58:45 -0300

    Adição de imagens e problema do chatgpt resolvido e melhorias no geral

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ pos_chatgpt_x = 500
 pos_chatgpt_y = 300
 
 # municao do acerto
-municao = pygame.image.load("fotos/pngwing.com.png").convert_alpha()
+municao = pygame.image.load("fotos/acerto.png").convert_alpha()
 municao = pygame.transform.scale(municao, (35, 35 ))
 pos_x_municao = 110
 pos_y_municao = 300
 vel_x_municao = 0
 
-pontos = 1
+pontos = 2
 
 triggered = False
 
@@ -53,14 +53,23 @@ rodando = True
 
 
 # definindo as colisoes
-def colisoes():
+def colisoes_erro():
     global pontos
     if monitor_rect.colliderect(erro_rect) or erro_rect.x == 60:
         pontos -= 1
         return True
     elif municao_rect.colliderect(erro_rect):
         pontos += 1
-
+        return True
+    else:
+        return False
+def colisoes_gpt():
+    global pontos
+    if monitor_rect.colliderect(chatgpt_rect) or chatgpt_rect.x == 60:
+        pontos -= 1
+        return True
+    elif municao_rect.colliderect(chatgpt_rect):
+        pontos += 1
         return True
     else:
         return False
@@ -68,7 +77,7 @@ def colisoes():
 # respawnar os inimigos
 def respawnar():
     x = 1350
-    y = random.randint(1,640)
+    y = random.randint(80,560)
     return [x,y]
 
 def respawnar_municao():
@@ -93,22 +102,22 @@ while rodando:
     #movimentacao do monitor
     tecla = pygame.key.get_pressed()
     if tecla[pygame.K_UP] and pos_monitor_y > 1:
-        pos_monitor_y -= 1
+        pos_monitor_y -= 5
         if not triggered:
-            pos_y_municao -= 1
+            pos_y_municao -= 5
 
     if tecla[pygame.K_DOWN] and pos_monitor_y < 665:
-        pos_monitor_y += 1
+        pos_monitor_y += 5
         if not triggered:
-            pos_y_municao += 1
+            pos_y_municao += 5
     if tecla[pygame.K_SPACE]:
         triggered = True
-        vel_x_municao = 1
+        vel_x_municao = 5
 
     #movimento
-    x -= 0.4
-    pos_erro_x -= 0.6
-    pos_chatgpt_x -= 0.5
+    x -= 0.8
+    pos_erro_x -= 1.2
+    pos_chatgpt_x -= 1.0
     pos_x_municao += vel_x_municao
     screen.blit(monitor,(pos_monitor_x, pos_monitor_y))
     screen.blit(erro, (pos_erro_x, pos_erro_y))
@@ -120,30 +129,50 @@ while rodando:
         pos_erro_x = respawnar()[0]
         pos_erro_y = respawnar()[1]
 
-    if pos_erro_x == 50 or colisoes():
+    if colisoes_erro():
         pos_erro_x = respawnar()[0]
         pos_erro_y = respawnar()[1]
+        pos_x_municao, pos_y_municao, triggered, vel_x_municao = respawnar_municao()
 
     if pos_x_municao == 1300:
         pos_x_municao, pos_y_municao, triggered, vel_x_municao = respawnar_municao()
 
-    if pos_chatgpt_x:
+    if pos_chatgpt_x == 50:
         pos_chatgpt_x = respawnar()[0]
         pos_chatgpt_y = respawnar()[1]
 
-    if pos_chatgpt_x or colisoes():
+    if colisoes_gpt():
         pos_chatgpt_x = respawnar()[0]
         pos_chatgpt_y = respawnar()[1]
+        pos_x_municao, pos_y_municao, triggered, vel_x_municao = respawnar_municao()
 
     # criando as fontes de pontuação
-    fonte = pygame.font.Font("fonte/ARCADECLASSIC.TTF", 30)
-    placar = fonte.render(f"Score {int(pontos)}", False, (0, 0, 0))
-    screen.blit(placar, (50,50))
+    estrela = pygame.image.load("fotos/estrela.png").convert_alpha()
+    estrela = pygame.transform.scale(estrela, (40, 40))
+    pos_estrela_x = 20
+    pos_estrela_y = 45
+    estrela_rect = estrela.get_rect()
+    screen.blit(estrela, (pos_estrela_x, pos_estrela_y))
+    estrela_rect.y = pos_estrela_y
+    estrela_rect.x = pos_estrela_x
+
+    relogio = pygame.image.load("fotos/relogio.png").convert_alpha()
+    relogio = pygame.transform.scale(relogio, (40, 40))
+    pos_relogio_x = 20
+    pos_relogio_y = 90
+    relogio_rect = relogio.get_rect()
+    screen.blit(relogio, (pos_relogio_x, pos_relogio_y))
+    relogio_rect.y = pos_relogio_y
+    relogio_rect.x = pos_relogio_x
+
+    fonte = pygame.font.Font("fonte/Arcades.ttf", 40)
+    placar = fonte.render(f"{pontos}", False, (0, 0, 0))
+    screen.blit(placar, (80,50))
 
     tempo_restante = 40 - int(time.time() - start_time)
-    fonte_tempo = pygame.font.Font("fonte/ARCADECLASSIC.TTF", 30)
-    texto_tempo = fonte_tempo.render(f"Tempo restante {tempo_restante}", False, (255, 255, 255))
-    screen.blit(texto_tempo, (50, 100))
+    fonte_tempo = pygame.font.Font("fonte/Arcades.ttf", 40)
+    texto_tempo = fonte_tempo.render(f"{tempo_restante}s", False, (0, 0, 0))
+    screen.blit(texto_tempo, (80, 100))
 
 
 
@@ -156,19 +185,19 @@ while rodando:
     screen.blit(monitor, (pos_monitor_x, pos_monitor_y))
 
     # criar o desenho
-    pygame.draw.rect(screen, (255, 0, 0), erro_rect, 4)
-    pygame.draw.rect(screen, (255, 0, 0), municao_rect, 4)
-    pygame.draw.rect(screen, (255, 0, 0), chatgpt_rect, 4)
-    pygame.draw.rect(screen, (255, 0, 0), monitor_rect, 4)
+   # pygame.draw.rect(screen, (255, 0, 0), erro_rect, 4)
+    #pygame.draw.rect(screen, (255, 0, 0), municao_rect, 4)
+    #pygame.draw.rect(screen, (255, 0, 0), chatgpt_rect, 4)
+    #pygame.draw.rect(screen, (255, 0, 0), monitor_rect, 4)
 
     monitor_rect.y = pos_monitor_y
     monitor_rect.x = pos_monitor_x
 
-    chatgpt_rect.y = pos_chatgpt_y
-    chatgpt_rect.x = pos_chatgpt_x
-
     erro_rect.y = pos_erro_y
     erro_rect.x = pos_erro_x
+
+    chatgpt_rect.y = pos_chatgpt_y
+    chatgpt_rect.x = pos_chatgpt_x
 
     municao_rect.y = pos_y_municao
     municao_rect.x = pos_x_municao
